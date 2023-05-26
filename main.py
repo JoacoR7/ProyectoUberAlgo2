23a3c943884f98c14aef9093f44bc1e789e192a1
paulisuden <spaulinaabril@gmail.com>
2023-05-26 13:01:26 -0300

    Ubicacion fija - Existe direccion

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uber
 import servicios.serializacion
 import servicios.MapaServicio as ms
+import servicios.UbicacionFijaServicio as uf
 
 #servicios.serializacion.serializarPersonas(["A", "B", "C"])
 cantidadV, aristas = servicios.serializacion.extraerMatriz()
 map = ms.createMap(cantidadV, aristas)
 print(ms.existPath(map, 7, 4))
-
+uf.crearUbiFija(cantidadV,map)
+uf.crearUbiFija(cantidadV,map)
 #uber.menu()
