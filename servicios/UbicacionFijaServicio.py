5020d10a31d723050feb665c00c3fa04472eaf5f
paulisuden <spaulinaabril@gmail.com>
2023-05-26 11:37:23 -0300

    Ubicacion Fija y Movil

--- servicios/UbicacionFijaServicio.py
+++ servicios/UbicacionFijaServicio.py
@@ -1,9 +1,52 @@
 import entidades.UbicacionFija as uf
 import servicios.DireccionServicio as ds
+import entidades.dictionary as dic
 
-def crearUbicacion(nombre, direccion):
-    u = uf.UbicacionFija()
-    return u
+#dic = [None]*7
+
+def crearUbiFija():
+    print("Ingrese el nombre de la ubicación fija: ")
+    nombre = input()
+    dir = ds.crearDireccion()
+    
+    if dir != False:
+        pos = calcularPos(nombre)
+        dic.insertInPos(dic, pos, nombre, dir)
+    else:
+        print("Dirección inválida")                   ###ver
+
+def searchUbiFija(dic,nombre): #dado el nombre de la ubicacion, busca la dirección (si es que existe)
+    pos = calcularPos(nombre)
+    if dic[pos] == None:
+        return False
+    else: #busco key
+        current = dic[pos].head
+        while current != None:
+            if current.key == nombre:
+                break
+            current = current.nextNode
+        if current == None:
+            return False
+        else:
+            return current.value #devuelve la dirección
 
 
+#search que dada una direccion, devolver si se encuentra algo ahí??
 
+def calcularPos(nombre):
+    letra = nombre[0]
+    if letra == "A":
+        pos = 0
+    elif letra == "E":
+        pos = 1
+    elif letra == "H":
+        pos = 2
+    elif letra == "I":
+        pos = 3
+    elif letra == "K":
+        pos = 4
+    elif letra == "S":
+        pos = 5
+    elif letra == "T":
+        pos = 6
+    return pos
