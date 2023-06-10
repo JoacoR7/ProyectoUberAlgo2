3a448cac12b06e1c82cc544b8263f9117fa6ece3
Joaquin Ruiz <70347742+JoacoR7@users.noreply.github.com>
2023-06-10 12:47:34 -0300

    Arreglos merge

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import entidades.graph as g
 
 import servicios.CargarDireccionServicio as cd
 #servicios.serializacion.serializarPersonas(["A", "B", "C"])
+
+
+
+
 datos = s.extraerEsquinasYCalles()
 aristas = ms.obtenerAristas(datos[1])
 esquinas = ms.obtenerEsquinas(datos[0])
@@ -15,24 +19,4 @@ map, mapAux = ms.crearMapa(esquinas, aristas)
 s.serializarMapa(map)
 mapa = s.buscarMapa()
 print("")
-ms.printMap(map)
-ms.printMap(mapAux)
-#g.shortestPath(map, mapAux, 7, 4) 
-print("")
-
-
-#print(ms.existPath(map, 7, 4))
-
-
-
-cd.cargarUbicacion(cantidadV,map)
-
-#uber.menu()
-
-
-
-ms.cálculosIniciales(map)
-
-#TODO: arreglar inserción de grafo, aunque no salgan aristas de un vértice, colocar datos en el slot correspondiente (al menos la key)
-#servicios.serializacion.serializarPersonas(["A", "B", "C"])
 
